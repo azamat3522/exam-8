6ef6689c18b86f9d1e459efa420f9f6dbbd3d1d0
User <User@Users-MacBook-Pro.local>
2019-11-16 14:35:45 +0600

    Создал страницы удаление и редактрования, и вывел картинку заглушку

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from webapp.views import ProductListView, ProductDetailView, ProductCreateView
+from webapp.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = 'webapp'
 
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete')
 
 ]
 
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from webapp.forms import ProductForm
 from webapp.models import Product
 
 
@@ -22,5 +23,22 @@ class ProductCreateView(CreateView):
     template_name = 'create.html'
     fields = ('name', 'category', 'description', 'image')
     success_url = reverse_lazy('webapp:index')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'update.html'
+    context_object_name = 'product'
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+        model = Product
+        template_name = 'delete.html'
+        context_object_name = 'product'
+        success_url = reverse_lazy('webapp:index')
 
 
