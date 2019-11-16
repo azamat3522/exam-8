18b9611928f2a28b7a76f866d9755c15adfac08a
User <User@Users-MacBook-Pro.local>
2019-11-16 13:47:20 +0600

    Создал страницы списка,просмотра и создания товара. Загрузил widget_tweaks и обновил requirements

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
+from webapp.views import ProductListView, ProductDetailView, ProductCreateView
+
 app_name = 'webapp'
 
 urlpatterns = [
-
+    path('', ProductListView.as_view(), name='index'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
 
 ]
 
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+
+from webapp.models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'index.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'detail.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'create.html'
+    fields = ('name', 'category', 'description', 'image')
+    success_url = reverse_lazy('webapp:index')
+
+
